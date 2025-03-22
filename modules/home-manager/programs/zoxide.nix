7b65255dcc6e289745ebf8fe868777f7b3fa5cc1
Kai R <stfourrtup@gmail.com>
2025-03-22 13:10:14 -0400

    zoxide fix (real)

--- modules/home-manager/programs/zoxide.nix
+++ modules/home-manager/programs/zoxide.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   programs.zoxide = {
     enable = true;
 
@@ -9,4 +9,10 @@
       "--cmd cd"
     ];
   };
+
+  programs.bash.enable = true;
+
+  home.packages = with pkgs; [
+    zoxide
+  ];
 }
