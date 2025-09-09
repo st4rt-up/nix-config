8f6c3bd02a18d6db87e3bf2a80700e545ef84344
Kai R <stfourrtup@gmail.com>
2025-09-09 12:38:01 -0400

    feature: screenshots work now through grim

--- modules/home/programs/wayland-utils.nix
+++ modules/home/programs/wayland-utils.nix
@@ -1,7 +1,15 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     wf-recorder
+
+    # screenshot utilities
     grim
     sway-contrib.grimshot
+  ];
+
+  wayland.windowManager.hyprland.settings.bind = [
+    ", Print, exec, grimshot --notify savecopy screen"
+    "Control_L , Print, exec, grimshot --notify savecopy area"
+
   ];
 }
