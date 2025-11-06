fe2995ee1421a2ab42fe8843433a63e59cb0815b
Kai R <stfourrtup@gmail.com>
2025-11-06 04:20:23 -0500

    addition: more niri config stuff

--- modules/programs/swayosd/default.nix
+++ modules/programs/swayosd/default.nix
@@ -1,33 +1,47 @@
 {
   username,
+  config,
+  lib,
   pkgs,
   ...
-}: {
-  environment.systemPackages = with pkgs; [
-    swayosd
-  ];
+}: let
+  inherit (lib) optionalAttrs;
+  outOfStore = config.home-manager.users.${username}.lib.file.mkOutOfStoreSymlink;
+  configPath = config.var.flake-path + "/modules/programs/swayosd/dotfiles";
+
+  niri-config = "swayosd-niri.kdl";
+in {
+  environment.systemPackages = with pkgs; [swayosd];
 
   home-manager.users.${username} = {
-    wayland.windowManager.hyprland.settings = {
-      exec-once = ["swayosd-server"];
-    };
-
     services.swayosd.enable = true;
 
-    wayland.windowManager.hyprland.settings.binde = [
-      ", $raiseVolume, exec, swayosd-client --output-volume raise"
-      ", $lowerVolume, exec, swayosd-client --output-volume lower"
+    xdg.configFile =
+      # ==== niri binds
+      optionalAttrs config.programs.niri.enable
+      {
+        "niri/config.kdl".text = "include \"${niri-config}\"";
+        "niri/${niri-config}".source = outOfStore configPath + "/${niri-config}";
+      };
 
-      "SHIFT, $raiseVolume, exec, swayosd-client --output-volume +10"
-      "SHIFT, $lowerVolume, exec, swayosd-client --output-volume -10"
+    # ==== hyprland binds
+    wayland.windowManager.hyprland.settings = {
+      exec-once = ["swayosd-server"];
+      binde = [
+        ", $raiseVolume, exec, swayosd-client --output-volume raise"
+        ", $lowerVolume, exec, swayosd-client --output-volume lower"
 
-      "CTRL, $raiseVolume, exec, swayosd-client --output-volume +1"
-      "CTRL, $lowerVolume, exec, swayosd-client --output-volume -1"
+        "SHIFT, $raiseVolume, exec, swayosd-client --output-volume +10"
+        "SHIFT, $lowerVolume, exec, swayosd-client --output-volume -10"
 
-      ", $muteVolume, exec, swayosd-client --output-volume mute-toggle"
+        "CTRL, $raiseVolume, exec, swayosd-client --output-volume +1"
+        "CTRL, $lowerVolume, exec, swayosd-client --output-volume -1"
 
-      ", $raiseBrightness, exec, swayosd-client --brightness raise"
-      ", $lowerBrightness, exec, swayosd-client --brightness lower"
-    ];
+        ", $muteVolume, exec, swayosd-client --output-volume mute-toggle"
+
+        ", $raiseBrightness, exec, swayosd-client --brightness raise"
+        ", $lowerBrightness, exec, swayosd-client --brightness lower"
+      ];
+    };
   };
 }
