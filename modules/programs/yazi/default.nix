6269d9ad0ed8e2b3896afcff810879a69b07bbde
Kai R <stfourrtup@gmail.com>
2025-11-20 14:46:55 -0500

    addition: added real config for yazi

--- modules/programs/yazi/default.nix
+++ modules/programs/yazi/default.nix
@@ -1,19 +1,19 @@
 {
   username,
   config,
+  pkgs,
   ...
 }: let
   configPath = config.var.flake-path + "/modules/programs/yazi/dotfiles";
   outOfStore = config.home-manager.users.${username}.lib.file.mkOutOfStoreSymlink;
 in {
-  programs.yazi.enable = true;
+  environment.systemPackages = with pkgs; [yazi];
+  environment.sessionVariables = {
+    YAZI_CONFIG_HOME = "~/.config/yazi";
+  };
 
   home-manager.users.${username} = {
     stylix.targets.yazi.enable = false;
-    home.file = {
-      # ".config/yazi/yazi.toml".source = outOfStore configPath + "yazi.toml";
-      ".config/yazi/keymap.toml".source = outOfStore configPath + "keymap.toml";
-      # ".config/yazi.theme.toml".source = outOfStore configPath + "theme.toml";
-    };
+    xdg.configFile."yazi".source = outOfStore configPath;
   };
 }
